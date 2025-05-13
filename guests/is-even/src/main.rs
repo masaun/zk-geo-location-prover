ac422f5a03075c6898d3c4f957a008c18fa3855f
masaun <masaun@protonmail.com>
2025-05-13 11:25:21 +0900

    [guest]: Define the acceptable and unacceptable geo-locations

--- guests/is-even/src/main.rs
+++ guests/is-even/src/main.rs
@@ -22,12 +22,19 @@ fn main() {
     // Read the input data for this application.
     let mut input_bytes = Vec::<u8>::new();
     env::stdin().read_to_end(&mut input_bytes).unwrap();
+    
     // Decode and parse the input
     let number = <U256>::abi_decode(&input_bytes, true).unwrap();
 
     // Run the computation.
     // In this case, asserting that the provided number is even.
     assert!(!number.bit(0), "number is not even");
+
+    /// @dev - Acceptable coordidates (x, y) for the location. This will be used for the constraint.
+    // Define acceptable and unacceptable geo-locations
+    let input_geo_location = (10, 20);       // Example input coordinates (x, y)
+    let unacceptable_geo_location = (0, 0);  // Example unacceptable coordinates (x, y)
+    assert!(input_geo_location != unacceptable_geo_location, "input_geo_location must be outside of unacceptable geo_location");
 
     // Commit the journal that will be received by the application contract.
     // Journal is encoded using Solidity ABI for easy decoding in the app contract.
