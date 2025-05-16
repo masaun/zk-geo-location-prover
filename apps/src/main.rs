9e887f2be506a3fdc23cc457d42205e9216707a9
masaun <masaun@protonmail.com>
2025-05-16 16:48:12 +0900

    [guest & guest test & apps]: Rename the name of the guest program from the 'zk-circuit-for-proving-geo-location' to the 'geo-location-prover'

--- apps/src/main.rs
+++ apps/src/main.rs
@@ -28,7 +28,7 @@ use boundless_market::{
     storage::StorageProviderConfig,
 };
 use clap::Parser;
-use guests::{ZK_CIRCUIT_FOR_PROVING_GEO_LOCATION_ELF, ZK_CIRCUIT_FOR_PROVING_GEO_LOCATION_ID};
+use guests::{GEO_LOCATION_PROVER_ELF, GEO_LOCATION_PROVER_ID};
 //use guests::{IS_EVEN_ELF, IS_EVEN_ID};
 use risc0_zkvm::{default_executor, sha::Digestible};
 use url::Url;
@@ -106,7 +106,7 @@ async fn main() -> Result<()> {
         boundless_client.storage_provider.is_some(),
         "a storage provider is required to upload the zkVM guest ELF"
     );
-    let image_url = boundless_client.upload_image(ZK_CIRCUIT_FOR_PROVING_GEO_LOCATION_ELF).await?;
+    let image_url = boundless_client.upload_image(GEO_LOCATION_PROVER_ELF).await?;
     //let image_url = boundless_client.upload_image(IS_EVEN_ELF).await?;
     tracing::info!("Uploaded image to {}", image_url);
 
@@ -132,7 +132,7 @@ async fn main() -> Result<()> {
     // It can also be useful to ensure the guest can be executed correctly and we do not send into
     // the market unprovable proving requests. If you have a different mechanism to get the expected
     // journal and set a price, you can skip this step.
-    let session_info = default_executor().execute(guest_env.try_into().unwrap(), ZK_CIRCUIT_FOR_PROVING_GEO_LOCATION_ELF)?;
+    let session_info = default_executor().execute(guest_env.try_into().unwrap(), GEO_LOCATION_PROVER_ELF)?;
     //let session_info = default_executor().execute(guest_env.try_into().unwrap(), IS_EVEN_ELF)?;
     let mcycles_count = session_info
         .segments
@@ -169,7 +169,7 @@ async fn main() -> Result<()> {
         .with_image_url(image_url.to_string())
         .with_input(request_input)
         .with_requirements(Requirements::new(
-            ZK_CIRCUIT_FOR_PROVING_GEO_LOCATION_ID,
+            GEO_LOCATION_PROVER_ID,
             //IS_EVEN_ID,
             Predicate::digest_match(journal.digest()),
         ))
@@ -216,7 +216,7 @@ async fn main() -> Result<()> {
 
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    /// [TODO]: Replace the following smart contract interaction code with the zk-circuit-for-proving-geo-location based smart contract interection code.  ///
+    /// [TODO]: Replace the following smart contract interaction code with the geo-location-prover based smart contract interection code.  ///
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     // // Interact with the EvenNumber contract by calling the set function with our number and
