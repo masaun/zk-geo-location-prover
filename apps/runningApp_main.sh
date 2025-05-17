4cc92414d7549509a01369f9cc46787917c6082f
masaun <masaun@protonmail.com>
2025-05-17 13:09:23 +0900

    [Deployment]: Modify the Deploy.s.sol and runningScript_Deploy.sh for deploying on BASE testnet

--- apps/runningApp_main.sh
+++ apps/runningApp_main.sh
@@ -5,5 +5,5 @@ echo "Update the guest program"
 cargo build
 
 echo "Running the app (./apps/src/main.rs) with the following environment variables:"
-RUST_LOG=info cargo run --bin app -- --geo-location-proof-verifier-address ${GEO_LOCATION_PROOF_VERIFIER_ADDRESS:?} --number 4
+RUST_LOG=info cargo run --bin app -- --geo-location-proof-verifier-address ${GEO_LOCATION_PROOF_VERIFIER_ADDRESS:?}
 #RUST_LOG=info cargo run --bin app -- --even-number-address ${GEO_LOCATION_PROOF_VERIFIER_ADDRESS:?} --number 4
--- contracts/scripts/runningScript_Deploy.sh
+++ contracts/scripts/runningScript_Deploy.sh
@@ -4,5 +4,26 @@ echo "Read the environment variables..."
 echo "Compile the contracts to be deployed..."
 forge build
 
-echo "Deploying the contract on Ethereum Sepolia testnet..."
-forge script contracts/scripts/Deploy.s.sol --rpc-url ${RPC_URL:?} --broadcast -vv
+# echo "Deploying the contract on Ethereum Sepolia testnet..."
+# forge script contracts/scripts/Deploy.s.sol --rpc-url ${RPC_URL:?} --broadcast -vv
+
+echo "Deploying the GeoLocationProofVerifier contract on Base Sepolia Testnet..."
+forge script contracts/scripts/Deploy.s.sol \
+    --broadcast \
+    --rpc-url ${BASE_TESTNET_RPC} \
+    --chain-id ${BASE_TESTNET_CHAIN_ID} \
+    --private-key ${BASE_TESTNET_PRIVATE_KEY} \
+    ./GeoLocationProofVerifier.sol:GeoLocationProofVerifier --skip-simulation --legacy
+
+# [NOTE - Adding the "--legacy" option]: Due to this error - Error: Failed to estimate EIP1559 fees. This chain might not support EIP1559, try adding --legacy to your command.
+
+echo "Verify the deployed-GeoLocationProofVerifier contract on Base Sepolia Testnet Explorer..."
+forge script contracts/scripts/Deploy.s.sol \
+    --rpc-url ${BASE_TESTNET_RPC} \
+    --chain-id ${BASE_TESTNET_CHAIN_ID} \
+    --private-key ${BASE_TESTNET_PRIVATE_KEY} \
+    --resume \
+    --verify \
+    --verifier etherscan \
+    --verifier-url https://api-sepolia.basescan.org/api \
+    --etherscan-api-key ${BASESCAN_API_KEY} \
